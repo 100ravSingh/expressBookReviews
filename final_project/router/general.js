59cf0cb4eb9e0f916470c9538283738f5e53cc2b
Sourav Singh <55342631+100ravSingh@users.noreply.github.com>
2025-11-30 17:30:00 +0000

    changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,16 +11,22 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const isbn = req.params.isbn;   // retrieve ISBN from URL
+  const book = books[isbn];       // find the book from the booksdb
+
+  if (book) {
+    return res.send(JSON.stringify(book, null, 4));
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
+});
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
